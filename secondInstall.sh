8be2d849b6c01fc2b9c840ae13e359b21e9aaeba
ivanmaulana <ivanmaulana@apps.ipb.ac.id>
2016-03-24 04:16:29 +0700

    add conf

---
+++ secondInstall.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+pear install Console_Getopt
+
+cd /usr/src
+wget https://iksemel.googlecode.com/files/iksemel-1.4.tar.gz
+tar xf iksemel-1.4.tar.gz
+cd iksemel-*
+./configure
+make
+make install
+
+cd /usr/src
+git clone http://code.cs.ipb.ac.id/ivanmaulana/asterisk.git
+
+cd asterisk
+
+tar -xjvf pjproject-2.4.tar.bz2
+rm -f pjproject-2.4.tar.bz2
+cd pjproject-2.4
+CFLAGS='-DPJ_HAS_IPV6=1' ./configure --enable-shared --disable-sound --disable-resample --disable-video --disable-opencore-amr
+make dep
+make
+make install
+cd ..
+
+tar vxfz jansson.tar.gz
+rm -f jansson.tar.gz
+cd jansson-*
+autoreconf -i
+./configure
+make
+make install
+cd ..
+
+tar xvfz asterisk-13-current.tar.gz
+rm -f asterisk-13-current.tar.gz
+cd asterisk-*
+contrib/scripts/install_prereq install
+./configure
+contrib/scripts/get_mp3_source.sh
+make menuselect
+
+make
+make install
+make config
+ldconfig
+update-rc.d -f asterisk remove
+
+cd /var/lib/asterisk/sounds
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-en-wav-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-wav-current.tar.gz
+tar xvf asterisk-core-sounds-en-wav-current.tar.gz
+rm -f asterisk-core-sounds-en-wav-current.tar.gz
+tar xfz asterisk-extra-sounds-en-wav-current.tar.gz
+rm -f asterisk-extra-sounds-en-wav-current.tar.gz
+# Wideband Audio download
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-core-sounds-en-g722-current.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/sounds/asterisk-extra-sounds-en-g722-current.tar.gz
+tar xfz asterisk-extra-sounds-en-g722-current.tar.gz
+rm -f asterisk-extra-sounds-en-g722-current.tar.gz
+tar xfz asterisk-core-sounds-en-g722-current.tar.gz
+rm -f asterisk-core-sounds-en-g722-current.tar.gz
+
+cd /usr/src/asterisk
+mv sip.conf /etc/asterisk/
+mv extensions.conf /etc/asterisk/
+
+asterisk
